37050cdebe0620e5591e5d246e6e6036653b0b56
manueladriancaceres <adriancaceres980@gmail.com>
2024-07-10 16:51:17 -0300

    initial proyect

---
+++ src/main/java/com/edutec/demoneo4j/Demoneo4jApplication.java
@@ -0,0 +1,13 @@
+package com.edutec.demoneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoneo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoneo4jApplication.class, args);
+	}
+
+}
